477ccfbd04cc9fbad1c9499f871febf39665ced7
Mtllns <miquelmtllns@pm.me>
2020-01-30 10:36:57 +0100

    FASE 3:
    Simplificado mostrar popup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([41.3866854, 2.1699935], 17);
+var map = L.map('map').setView([41.3866854, 2.1699935], 17);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	maxZoom: 18,
@@ -6,16 +6,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 	'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 	'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	id: 'mapbox/streets-v11'
-}).addTo(mymap);
+}).addTo(map);
 
-L.marker([41.3866569, 2.1659264]).addTo(mymap);
-
-var popup = L.popup();
-
-function onMapClick(e) {
-	popup
-		.setLatLng(e.latlng)
-		.setContent("Calle Calle Balmes, 16.<br>08007 Barcelona ")
-		.openOn(mymap);
-}
-mymap.on('click', onMapClick);
+var marker = L.marker([41.3866569, 2.1659264]).addTo(map); // añade marcador en la Lat y  long
+marker.bindPopup("Calle Calle Balmes, 16.<br>08007 Barcelona").openPopup(); // añade popup al hacer clic en el marcador

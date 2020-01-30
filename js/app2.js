3c863795586cee69a2591a81bbbfe85f43b8d7a4
Mtllns <miquelmtllns@pm.me>
2020-01-30 12:16:54 +0100

    FASE 2:
    Crea marcador con popup en cada clic, y elimina el anterior.

--- js/app2.js
+++ js/app2.js
@@ -1,22 +1,29 @@
-var mymap = L.map('mapid').setView([41.3866854, 2.1699935], 15);
+let map = L.map('map').setView([41.3866854, 2.1699935], 15);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	maxZoom: 22,
+	maxZoom: 19,
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 	'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 	'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	id: 'mapbox/streets-v11'
-}).addTo(mymap);
+}).addTo(map);
 
-// L.marker([41.3866569, 2.1659264]).addTo(mymap);
+createMarker();
 
-var popup = L.popup();
+function createMarker() {
+    var marker = {};
 
-function onMapClick(e) {
-	popup
-		.setLatLng(e.latlng)
-		.setContent("Mis coordenadas son: <br>"+e.latlng.toString())
-		.openOn(mymap);
+    map.on('click', function(e) {
+        latitude = e.latlng.lat;
+        longitude = e.latlng.lng;
+
+        if (marker != undefined) {
+            map.removeLayer(marker);
+        };
+
+        marker = L.marker([latitude, longitude]).addTo(map);
+        var group = new L.featureGroup([marker]);
+        map.fitBounds(group.getBounds());
+        marker.bindPopup("Mis coordenadas son: <br>" + "<br>Lat: " + latitude + " Lng: " + longitude+"</br>").openPopup();
+	});
 }
-mymap.on('click', onMapClick);
-
